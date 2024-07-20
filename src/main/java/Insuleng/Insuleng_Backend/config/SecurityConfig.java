09461288d83c30f6f16aa3e5ee71e33385c2c298
eclipse021 <junhyeon@naver.com>
2024-07-20 18:00:23 +0900

    Cors 문제해결을 위해 SecurityConfig 설정 및 CorsMvcConfig 생성

--- src/main/java/Insuleng/Insuleng_Backend/config/SecurityConfig.java
+++ src/main/java/Insuleng/Insuleng_Backend/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package Insuleng.Insuleng_Backend.config;
 import Insuleng.Insuleng_Backend.jwt.JWTFilter;
 import Insuleng.Insuleng_Backend.jwt.JWTUtil;
 import Insuleng.Insuleng_Backend.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -46,6 +52,27 @@ public class SecurityConfig {
 
         //bearer방식을 쓰기 위해 basic disable 하기
         http.httpBasic((basic)->basic.disable());
+
+        http.cors((cors) -> cors
+                        .configurationSource(new CorsConfigurationSource() {
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                                CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+                                corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                                corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+                                corsConfiguration.setAllowCredentials(true);
+                                corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                                corsConfiguration.setMaxAge(3600L);
+
+                                corsConfiguration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                                return corsConfiguration;
+                            }
+                        })
+
+                );
 
         http.authorizeHttpRequests((auth) -> auth
                 .requestMatchers("/user/**").authenticated()
