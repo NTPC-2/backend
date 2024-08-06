2296afe99434add5802106955ca7e73a9ba8ee8c
eclipse021 <junhyeon@naver.com>
2024-08-06 21:40:32 +0900

    ReviewFormDto 추가

---
+++ src/main/java/Insuleng/Insuleng_Backend/src/restaurant/dto/ReviewFormDto.java
@@ -0,0 +1,23 @@
+package Insuleng.Insuleng_Backend.src.restaurant.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Schema(description = "리뷰 form Dto(만약 기본 리뷰에 사진이 없으면 reviewImg에 null이 출력된다)")
+public class ReviewFormDto {
+
+    private Double star;
+    private String contents;
+    List<String> reviewImg;
+
+    public ReviewFormDto(String contents, Double star){
+        this.contents = contents;
+        this.star = star;
+    }
+}
